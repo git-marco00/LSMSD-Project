6c5b97516101baf581ed70bbd317613b912cb027
federic0casu <federicocasu.mail@gmail.com>
2022-12-20 11:31:26 +0100

    Upload first version of project structure

---
+++ BGnet/src/main/java/it/unipi/BGnet/BGnetApplication.java
@@ -0,0 +1,14 @@
+package it.unipi.BGnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BGnetApplication
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(BGnetApplication.class, args);
+	}
+
+}
---
+++ BGnet/src/test/java/it/unipi/BGnet/BGnetApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unipi.BGnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BGnetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
