2fe2760ecd80000fc226202807f3221b7f2fcd3d
Sgazzirro <angelodemarco0202@gmail.com>
2023-01-19 11:27:21 +0100

    search page completed with some errors on css

---
+++ BGnet/src/main/java/it/unipi/BGnet/controllers/searchPageController.java
@@ -0,0 +1,12 @@
+package it.unipi.BGnet.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class searchPageController {
+    @RequestMapping("/searchPage")
+    public String gamePage(){
+        return "searchPage";
+    }
+}
