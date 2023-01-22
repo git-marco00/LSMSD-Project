c72aa1844125ef175416d3962fc0f41f81a68f31
Sgazzirro <angelodemarco0202@gmail.com>
2023-01-22 14:37:56 +0100

    now admin page and tournament page are disabled for unregistered

--- BGnet/src/main/java/it/unipi/BGnet/controllers/AdminController.java
+++ BGnet/src/main/java/it/unipi/BGnet/controllers/AdminController.java
@@ -4,14 +4,18 @@ import it.unipi.BGnet.Utilities.SessionVariables;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
+@SessionAttributes("sessionVariables")
 public class AdminController {
     @RequestMapping("/adminPage")
     public String admin(Model model)
     {
         if((SessionVariables) model.getAttribute("sessionVariables") == null)
             model.addAttribute("sessionVariables", new SessionVariables());
+        if(!((SessionVariables) model.getAttribute("sessionVariables")).admin)
+            return "login";
         return "adminPage";
     }
 }
--- BGnet/src/main/java/it/unipi/BGnet/controllers/TournamentController.java
+++ BGnet/src/main/java/it/unipi/BGnet/controllers/TournamentController.java
@@ -1,10 +1,17 @@
 package it.unipi.BGnet.controllers;
 
+import it.unipi.BGnet.Utilities.SessionVariables;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
+@SessionAttributes("sessionVariables")
 public class TournamentController {
     @RequestMapping("/tournamentPage")
-    public String tournamentPage(){return "tournamentPage";}
+    public String tournamentPage(Model model){
+        if(((SessionVariables) model.getAttribute("sessionVariables")).myself == null)
+            return "login";
+        return "tournamentPage";}
 }
